141a2710acc55371d553f9a1cf1662dcd5f1bf80
imabee101 <andrew@drewcloud.ca>
2019-04-22 23:15:06 -0600

    react

---
+++ precache-manifest.9d9adffab3de377bc22a5ecdd5dca950.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a47e06e0cb67c1cd461",
+    "url": "/static/js/main.0a47e06e.chunk.js"
+  },
+  {
+    "revision": "6779d3220dd4e76decd8",
+    "url": "/static/js/1.6779d322.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4441102239a3d4fdaeadc2e1a90e802e",
+    "url": "/index.html"
+  }
+];
